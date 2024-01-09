5e4c07574166cb190f6e16ea661b9f5ee7aea391
HyunyoungA <dome96@naver.com>
2024-01-10 01:44:44 +0900

    20240110 모달창 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,6 +3,16 @@ console.log("파일연결성공");
 //	$("#header_wrap").load("header.html");
 //});
 
+//모달창 오픈
+var modal = document.querySelector("#modal_wrap");
+function init(){
+	modal.classList.remove("hidden");
+}
+//모달창 숨기기
+$(document).on("click", "#modal .content .con_btn_wrap .modal_btn, #modal .title span", function () {
+	$(".reset").val('');
+     modal.classList.add("hidden");
+});
 function loadHTML(file){
 console.log("연결성공");
     var rawFile = new XMLHttpRequest();
